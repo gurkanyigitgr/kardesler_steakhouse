4de901439dbbec01e171b13ad93aa466011d13a7
Gürkan <gurkanyigitgr@gmail.com>
2023-10-16 13:39:02 +0300

    add pages

--- app/page.js
+++ app/page.js
@@ -1,9 +1,33 @@
-import Image from "next/image";
+//components
+
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Steaks from './components/Steaks';
+import About from './components/About';
+import Why from './components/Why';
+import Testimonials from './components/Testimonials';
+import Cta from './components/Cta';
+import Footer from './components/Footer';
+import BackToTopBtn from './components/BackToTopBtn';
+import Fixed from './components/Fixed';
+import GoogleMap from './components/GoogleMap';
+import Gallery from './components/Gallery';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Kardeşler Steakhouse</h1>
-    </div>
+    <main className='max-w-[1920px] bg-whiteColor mx-auto relative overflow-hidden'>
+      <Header />
+      <Hero />
+      <Steaks />
+      <Why />
+      <Fixed />
+      <About />
+      <Gallery />
+      <Testimonials />
+      <GoogleMap />
+      <Cta />
+      <Footer />
+      <BackToTopBtn />
+    </main>
   );
 }
