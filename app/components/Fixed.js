56da312b31203ff9978d7e51069368243dedeb37
Gürkan <gurkanyigitgr@gmail.com>
2023-10-16 16:40:52 +0300

    fix css for fixed components

--- app/components/Fixed.js
+++ app/components/Fixed.js
@@ -2,10 +2,10 @@ export default function Fixed() {
   return (
     <section className='section flex flex-col items-center p-0 bg-gradient-to-tl from-[#b2b7c2]/10 via-[#b2b7c2]/10 to-slate-300 dark:bg-black dark:from-black dark:via-black dark:to-black'>
       <div className='mt-20 h-full w-full flex flex-col items-center'>
-        <div className='bg-fixed bg-no-repeat bg-cover bg-[center_center] h-full w-full text-[white] text-center table bg-1 '>
+        <div className='bg-fixed bg-no-repeat bg-cover bg-[center_center] h-[200px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-full w-full text-[white] text-center table bg-1 '>
           <h1 className='text-white table-cell align-middle h1'>Misyon</h1>
         </div>
-        <div className=' py-10 p-5 flex items-center justify-center'>
+        <div className='py-10 p-5 flex items-center justify-center'>
           <p className='text-gray-600 max-w-3xl text-center p xl:text-xl mx-auto dark:text-gray-300'>
             Müşterilerimize geleneksel Türk mutfağının en lezzetli ve özgün
             haliyle buluştuğu bir deneyim sunarak, unutulmaz anlar yaşatmak ve
@@ -13,7 +13,7 @@ export default function Fixed() {
           </p>
         </div>
 
-        <div className='bg-fixed bg-no-repeat bg-cover bg-[right_right] h-full w-full text-[white] text-center table bg-2'>
+        <div className='bg-fixed bg-no-repeat bg-cover bg-[right_right] h-[200px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-full w-full text-[white] text-center table bg-2'>
           <h1 className='text-white h1 table-cell align-middle'>Vizyon</h1>
         </div>
         <div className='py-10 p-5 flex items-center justify-center'>
